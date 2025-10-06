544ccd705c38a211b1bb717c0e2e4720227396f0
Mateus Pereira <mateusdeap@gmail.com>
2025-10-06 17:09:49 -0300

    Show "Sent" date in a readable format (#34)

--- app/helpers/puzzles_helper.rb
+++ app/helpers/puzzles_helper.rb
@@ -1,2 +1,5 @@
 module PuzzlesHelper
+  def time_ago(past_time)
+    "#{time_ago_in_words(past_time)} ago"
+  end
 end
--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -2,4 +2,6 @@ class Puzzle < ApplicationRecord
   enum :answer, ruby: 0, rails: 1
   enum :state, { approved: 0, rejected: 1, pending: 2, archived: 3 }
   has_many :answers
+
+  scope :archived, -> { where(state: :archived).order(sent_at: :desc) }
 end
