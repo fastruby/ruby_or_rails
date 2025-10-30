6872e91b43b5f7582ce5ce727c2af8c7627a3440
Julio Lucero <poramo@gmail.com>
2025-10-30 09:48:45 -0300

    Use FastRuby Styleguide

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+// This file is required by Sprockets (pulled in by styleguide gem dependencies)
+// Propshaft actually serves the assets, this manifest is just to prevent Sprockets errors
+// JavaScript is handled by importmap, not the asset pipeline
+//= link_tree ../images
+//= link application.css
+
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,7 +1,20 @@
 # Be sure to restart your server when you modify this file.
 
-# Version of your assets, change this if you want to expire all your assets.
-Rails.application.config.assets.version = "1.0"
+# Propshaft handles CSS/images automatically - no configuration needed here
+# JavaScript is handled by importmap, not the asset pipeline
 
-# Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path
+# Prevent Sprockets from complaining about JavaScript files in production
+# (they're handled by importmap, not the asset pipeline)
+if defined?(Sprockets) &&
+   Rails.application.config.respond_to?(:assets) &&
+   Rails.application.config.assets.respond_to?(:precompile)
+  # Add all JavaScript files that importmap might reference
+  # These are placeholders so Sprockets is satisfied; importmap serves them
+  Rails.application.config.assets.precompile += %w[
+    application.js
+    controllers/application.js
+    controllers/hello_controller.js
+    controllers/index.js
+    controllers/modal_controller.js
+  ]
+end
