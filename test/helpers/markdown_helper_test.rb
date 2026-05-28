3dc66fc05eaddfb80ef8e56fbc5e2d8dda90f8ee
Ariel Juodziukynas <arieljuod@gmail.com>
2026-05-28 16:49:40 -0300

    IIRR-34: Render puzzles's code with code-style

---
+++ test/helpers/markdown_helper_test.rb
@@ -0,0 +1,27 @@
+require "test_helper"
+
+class MarkdownHelperTest < ActiveJob::TestCase
+  include ApplicationHelper
+
+  test "renders `code` with code tag" do
+    puzzle_content = "Some content with `some code` in it"
+    as_html = markdown(puzzle_content)
+
+    assert_includes as_html, "<code>some code</code>"
+  end
+
+  test "renders ```code``` with code block" do
+    puzzle_content = <<~CONTENT
+    Some content with
+
+    ```
+    class ACodeBlock
+    end
+    ```
+    in it
+    CONTENT
+    as_html = markdown(puzzle_content)
+
+    assert_includes as_html, "<pre><code>class ACodeBlock"
+  end
+end
