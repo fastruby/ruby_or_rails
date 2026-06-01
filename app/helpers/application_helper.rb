62de5bd233dcd6f49d02e6675f8b1b59c9f6ab73
Ariel Juodziukynas <arieljuod@gmail.com>
2026-06-01 11:16:11 -0300

    IIRR-34: Fix newlines around codeblocks for Redcarpet renderer (#94)
    * IIRR-34: Fix newlines around codeblocks for Redcarpet renderer
    
    * IIRR-34: Fix bug with regexp that fixes newlines eating last character

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,14 @@ module ApplicationHelper
   def markdown(text)
     renderer = Redcarpet::Render::HTML.new(escape_html: true)
     markdown = Redcarpet::Markdown.new(renderer, fenced_code_blocks: true)
+
+    # Redcarpet needs the codeblocks to have 2 newlines before them
+    # fixing this at render because it's not really required by Discord
+    # it's just a quirk of Redcarpet
+    if text.match?(/[^\n]\n```(.*)\n```/m)
+      text = text.gsub(/([^\n])\n```(.*?)\n```/m, "\\1\n\n```\\2\n```")
+    end
+
     markdown.render(text.to_s).html_safe
   end
 end
